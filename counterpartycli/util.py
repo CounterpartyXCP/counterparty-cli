580ab97b17dda29653178f80fc82496ba9eeda6a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 16:22:49 +0000

    Adding tarfile member sanitization to extractall()

--- counterpartycli/util.py
+++ counterpartycli/util.py
@@ -158,7 +158,26 @@ def bootstrap(testnet=False, overwrite=True, ask_confirmation=False, quiet=False
 
     print('Extracting to "%s"...' % data_dir)
     with tarfile.open(TARBALL_PATH, 'r:gz') as tar_file:
-        tar_file.extractall(path=data_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar_file, path=data_dir)
 
     assert os.path.exists(DATABASE_PATH)
     os.chmod(DATABASE_PATH, 0o660)
